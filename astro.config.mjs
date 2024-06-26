580d486873674eaa8f1a2a8d94ca09172c8c310e
Jose <tamenis@outlook.com>
2024-06-26 14:43:12 +0200

    Falta de ortografia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+//import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  //output: 'server',
+  //adapter: netlify(),
 });
